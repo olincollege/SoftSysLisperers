ef128ad179ab1c265114143f51db4faaeea57ad7
DanPark13 <dpark0703@gmail.com>
2022-03-13 21:15:32 -0400

    Completed Evaluation
    - Takes in form of Polish Notation Grammar
    - Rather than returning regex abstract syntax tree
    - Returns evaluated expression

--- main.c
+++ main.c
@@ -11,6 +11,60 @@
 #include <editline/readline.h>
 #include <editline/history.h>
 
+#ifdef _WIN32
+
+static char buffer[2048];
+
+char* readline(char* prompt){
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = "\0";
+  return cpy;
+}
+
+#else
+#endif
+
+// Get operator string 
+long eval_op(long x, char* op, long y){
+  if(strcmp(op, "+") == 0){
+    return x + y;
+  }
+  else if(strcmp(op, "-") == 0){
+    return x - y;
+  }
+  else if(strcmp(op, "-") == 0){
+    return x * y;
+  }
+  else if(strcmp(op, "/") == 0){
+    return x / y;
+  }
+  return 0;
+}
+
+// Evaluate function based on input
+long eval(mpc_ast_t* t){
+  // If tagged as number, return it directly
+  if(strstr(t->tag, "number")){
+    return atoi(t->contents);
+  }
+
+  // Operator is always second child
+  char *operator = t->children[1]->contents;
+
+  // Store third child in 'x'
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while(strstr(t->children[i]->tag, "expression")){
+    x = eval_op(x, operator, eval(t->children[i]));
+    i++;
+  }
+  return x;
+}
+
 int main(int argc, char** argv) {
 
   // Create Parsers: number, operator, expression, and lispy
@@ -48,14 +102,15 @@ int main(int argc, char** argv) {
     // Parse Unit Input
     mpc_result_t r;
     if(mpc_parse("<stdin>", input, lsp, &r)){
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
+      // mpc_ast_print(r.output);
       mpc_ast_delete(r.output);
     }
     else{
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
     }
-
     // Free retrieved input
     free(input);
   }
