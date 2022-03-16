5534b5167b7a65cc4ea37f442775f30e5fe4cebe
DanPark13 <dpark0703@gmail.com>
2022-03-16 00:57:03 -0400

    Q-Expressions Implementation Base

--- main.c
+++ main.c
@@ -139,6 +139,18 @@ lval* lval_pop(lval* v, int i) {
   return x;
 }
 
+lval* lval_join(lval* x, lval* y) {
+
+  /* For each cell in 'y' add it to 'x' */
+  while (y->count) {
+    x = lval_add(x, lval_pop(y, 0));
+  }
+
+  /* Delete the empty 'y' and return 'x' */
+  lval_del(y);
+  return x;
+}
+
 // Deletes list that has extracted the element from
 lval* lval_take(lval* v, int i) {
   lval* x = lval_pop(v, i);
@@ -188,6 +200,72 @@ void lval_println(lval* v) {
   putchar('\n');
 }
 
+#define LASSERT(args, cond, err) \
+  if (!(cond)) { lval_del(args); return lval_err(err); }
+
+lval* lval_eval(lval* v);
+
+// Converts input S-Expression to Q-Expression
+lval* builtin_list(lval* a) {
+  a->type = LVAL_QEXPR;
+  return a;
+}
+
+lval* builtin_head(lval* a) {
+  LASSERT(a, a->count == 1,
+    "Function 'head' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+    "Function 'head' passed incorrect type!");
+  LASSERT(a, a->cell[0]->count != 0,
+    "Function 'head' passed {}!");
+
+  lval* v = lval_take(a, 0);
+  while (v->count > 1) { lval_del(lval_pop(v, 1)); }
+  return v;
+}
+
+lval* builtin_tail(lval* a) {
+  LASSERT(a, a->count == 1,
+    "Function 'tail' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+    "Function 'tail' passed incorrect type!");
+  LASSERT(a, a->cell[0]->count != 0,
+    "Function 'tail' passed {}!");
+
+  lval* v = lval_take(a, 0);
+  lval_del(lval_pop(v, 0));
+  return v;
+}
+
+// Takes some input single Q-expression, converts into S-Expression, and evaluates
+lval* builtin_eval(lval* a) {
+  LASSERT(a, a->count == 1,
+    "Function 'eval' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+    "Function 'eval' passed incorrect type!");
+
+  lval* x = lval_take(a, 0);
+  x->type = LVAL_SEXPR;
+  return lval_eval(x);
+}
+
+lval* builtin_join(lval* a) {
+
+  for (int i = 0; i < a->count; i++) {
+    LASSERT(a, a->cell[i]->type == LVAL_QEXPR,
+      "Function 'join' passed incorrect type.");
+  }
+
+  lval* x = lval_pop(a, 0);
+
+  while (a->count) {
+    x = lval_join(x, lval_pop(a, 0));
+  }
+
+  lval_del(a);
+  return x;
+}
+
 lval* builtin_op(lval* a, char* op) {
   
   // Ensure all arguments are numbers 
@@ -234,6 +312,17 @@ lval* builtin_op(lval* a, char* op) {
   return x;
 }
 
+lval* builtin(lval* a, char* func) {
+  if (strcmp("list", func) == 0) { return builtin_list(a); }
+  if (strcmp("head", func) == 0) { return builtin_head(a); }
+  if (strcmp("tail", func) == 0) { return builtin_tail(a); }
+  if (strcmp("join", func) == 0) { return builtin_join(a); }
+  if (strcmp("eval", func) == 0) { return builtin_eval(a); }
+  if (strstr("+-/*", func)) { return builtin_op(a, func); }
+  lval_del(a);
+  return lval_err("Unknown Function!");
+}
+
 lval* lval_eval(lval* v);
 
 lval* lval_eval_sexpr(lval* v) {
@@ -262,7 +351,7 @@ lval* lval_eval_sexpr(lval* v) {
   }
   
   // Call builtin with operator
-  lval* result = builtin_op(v, f->sym);
+  lval* result = builtin(v, f->sym);
   lval_del(f);
   return result;
 }
@@ -281,79 +370,6 @@ lval* lval_read_num(mpc_ast_t* t) {
   long x = strtol(t->contents, NULL, 10);
   return errno != ERANGE ?
     lval_num(x) : lval_err("invalid number");
-}
-
-lval* builtin_head(lval* a) {
-  LASSERT(a, a->count == 1,
-    "Function 'head' passed too many arguments!");
-  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
-    "Function 'head' passed incorrect type!");
-  LASSERT(a, a->cell[0]->count != 0,
-    "Function 'head' passed {}!");
-
-  lval* v = lval_take(a, 0);
-  while (v->count > 1) { lval_del(lval_pop(v, 1)); }
-  return v;
-}
-
-lval* builtin_tail(lval* a) {
-  LASSERT(a, a->count == 1,
-    "Function 'tail' passed too many arguments!");
-  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
-    "Function 'tail' passed incorrect type!");
-  LASSERT(a, a->cell[0]->count != 0,
-    "Function 'tail' passed {}!");
-
-  lval* v = lval_take(a, 0);
-  lval_del(lval_pop(v, 0));
-  return v;
-}
-
-// Converts input S-Expression to Q-Expression
-lval* builtin_list(lval* a) {
-  a->type = LVAL_QEXPR;
-  return a;
-}
-
-// Takes some input single Q-expression, converts into S-Expression, and evaluates
-lval* builtin_eval(lval* a) {
-  LASSERT(a, a->count == 1,
-    "Function 'eval' passed too many arguments!");
-  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
-    "Function 'eval' passed incorrect type!");
-
-  lval* x = lval_take(a, 0);
-  x->type = LVAL_SEXPR;
-  return lval_eval(x);
-}
-
-lval* builtin_join(lval* a) {
-
-  for (int i = 0; i < a->count; i++) {
-    LASSERT(a, a->cell[i]->type == LVAL_QEXPR,
-      "Function 'join' passed incorrect type.");
-  }
-
-  lval* x = lval_pop(a, 0);
-
-  while (a->count) {
-    x = lval_join(x, lval_pop(a, 0));
-  }
-
-  lval_del(a);
-  return x;
-}
-
-lval* lval_join(lval* x, lval* y) {
-
-  /* For each cell in 'y' add it to 'x' */
-  while (y->count) {
-    x = lval_add(x, lval_pop(y, 0));
-  }
-
-  /* Delete the empty 'y' and return 'x' */
-  lval_del(y);
-  return x;
 }
 
 lval* lval_read(mpc_ast_t* t) {
